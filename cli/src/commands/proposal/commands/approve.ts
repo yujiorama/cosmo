373bdedc8c71bb68bcf0a7159f539e95be52bcdb
Yuji Okazawa <yujiorama+github@gmail.com>
2025-08-31 13:21:25 +0900

    approve.ts の作成

---
+++ cli/src/commands/proposal/commands/approve.ts
@@ -0,0 +1,49 @@
+import { Command, program } from 'commander';
+import ora from 'ora';
+import pc from 'picocolors';
+import { getBaseHeaders } from '../../../core/config.js';
+import { BaseCommandOptions } from '../../../core/types/types.js';
+import { handleProposalResult } from '../../../handle-proposal-result.js';
+
+export default (opts: BaseCommandOptions) => {
+  const command = new Command('approve');
+  command.description('Approves an existing proposal for a federated graph.');
+  command.argument('<name>', 'The name of the proposal to approve.');
+  command.requiredOption(
+    '-f, --federation-graph <federatedGraphName>',
+    'The name of the federated graph this proposal is for.',
+  );
+  command.option('-n, --namespace [string]', 'The namespace of the federated graph.');
+
+  command.action(async (name, options) => {
+    const spinner = ora(`Approving proposal: ${name}...`).start();
+
+    const resp = await opts.client.platform.approveProposal(
+      {
+        proposalName: name,
+        federatedGraphName: options.federationGraph,
+        namespace: options.namespace,
+      },
+      {
+        headers: getBaseHeaders(),
+      },
+    );
+
+    spinner.stop();
+
+    const result = handleProposalResult(resp, name, true);
+
+    if (result.success) {
+      if (result.message) {
+        console.log(result.message);
+      }
+    } else {
+      if (result.message) {
+        console.error(result.message);
+      }
+      process.exitCode = 1;
+    }
+  });
+
+  return command;
+};
