c6f69b64b1104512aa7153795046f5c989888eb6
Yuji Okazawa <yujiorama+github@gmail.com>
2025-08-31 13:24:39 +0900

    reject.ts の作成

---
+++ cli/src/commands/proposal/commands/reject.ts
@@ -0,0 +1,49 @@
+import { Command, program } from 'commander';
+import ora from 'ora';
+import pc from 'picocolors';
+import { getBaseHeaders } from '../../../core/config.js';
+import { BaseCommandOptions } from '../../../core/types/types.js';
+import { handleProposalResult } from '../../../handle-proposal-result.js';
+
+export default (opts: BaseCommandOptions) => {
+  const command = new Command('reject');
+  command.description('Rejects an existing proposal for a federated graph.');
+  command.argument('<name>', 'The name of the proposal to reject.');
+  command.requiredOption(
+    '-f, --federation-graph <federatedGraphName>',
+    'The name of the federated graph this proposal is for.',
+  );
+  command.option('-n, --namespace [string]', 'The namespace of the federated graph.');
+
+  command.action(async (name, options) => {
+    const spinner = ora(`Rejecting proposal: ${name}...`).start();
+
+    const resp = await opts.client.platform.rejectProposal(
+      {
+        proposalName: name,
+        federatedGraphName: options.federationGraph,
+        namespace: options.namespace,
+      },
+      {
+        headers: getBaseHeaders(),
+      },
+    );
+
+    spinner.stop();
+
+    const result = handleProposalResult(resp, name, false);
+
+    if (result.success) {
+      if (result.message) {
+        console.log(result.message);
+      }
+    } else {
+      if (result.message) {
+        console.error(result.message);
+      }
+      process.exitCode = 1;
+    }
+  });
+
+  return command;
+};
